20b61069da32840727dd862fe476a93e19901ef2
Andrew Kim <andrew75313@gmail.com>
2025-05-30 18:50:46 +0900

    refactor: remove HATEOAS in User Controller

--- src/main/java/com/example/toytalk/domain/users/controller/UserController.java
+++ src/main/java/com/example/toytalk/domain/users/controller/UserController.java
@@ -1,14 +1,11 @@
 package com.example.toytalk.domain.users.controller;
 
-import com.example.toytalk.domain.users.assembler.UserModelAssembler;
 import com.example.toytalk.domain.users.dto.SignupRequestDTO;
 import com.example.toytalk.domain.users.dto.UserResponseDTO;
 import com.example.toytalk.domain.users.dto.UserUpdateRequestDTO;
 import com.example.toytalk.domain.users.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,16 +13,12 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
-    private final UserModelAssembler userModelAssembler;
 
     @PostMapping("/signup")
     public ResponseEntity<UserResponseDTO> createUser(@Valid @RequestBody SignupRequestDTO requestDTO) {
@@ -33,59 +26,17 @@ public class UserController {
         return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
     }
 
-    /**
-     * HATEOAS 적용
-     * @param userId
-     * @return Activated 사용자 반환
-     */
     @GetMapping("/{userId}")
-    public ResponseEntity<EntityModel<UserResponseDTO>> getUser(@PathVariable UUID userId) {
+    public ResponseEntity<UserResponseDTO> getUser(@PathVariable UUID userId) {
         UserResponseDTO responseDTO = userService.getUser(userId);
-
-        EntityModel<UserResponseDTO> userModel = userModelAssembler.toModel(responseDTO);
-
-        return new ResponseEntity<>(userModel ,HttpStatus.OK);
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 
-    /**
-     * HATEOAS 적용
-     * @return Activated 사용자 반환
-     */
     @GetMapping("")
-    public ResponseEntity<CollectionModel<EntityModel<UserResponseDTO>>> getAllUsers() {
+    public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
         List<UserResponseDTO> responseDTOList = userService.getAllUsers();
-
-        List<EntityModel<UserResponseDTO>> userModels = responseDTOList.stream()
-                .map(userModelAssembler::toModel)
-                .toList();
-
-        CollectionModel<EntityModel<UserResponseDTO>> collectionModel =
-                CollectionModel.of(userModels,
-                        linkTo(methodOn(UserController.class).getAllUsers()).withSelfRel());
-
-        return new ResponseEntity<>(collectionModel, HttpStatus.OK);
+        return new ResponseEntity<>(responseDTOList, HttpStatus.OK);
     }
-
-    /**
-     * HATEOAS 미적용
-     * @param userId
-     * @return Activated 사용자 반환
-     */
-//    @GetMapping("/{userId}")
-//    public ResponseEntity<UserResponseDTO> getUser(@PathVariable UUID userId) {
-//        UserResponseDTO responseDTO = userService.getUser(userId);
-//        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
-//    }
-
-    /**
-     * HATEOAS 미적용
-     * @return Activated 사용자 반환
-     */
-//    @GetMapping("")
-//    public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
-//        List<UserResponseDTO> responseDTOList = userService.getAllUsers();
-//        return new ResponseEntity<>(responseDTOList, HttpStatus.OK);
-//    }
 
     @PutMapping("/{userId}/update")
     public ResponseEntity<UserResponseDTO> updateUser(@PathVariable UUID userId,
